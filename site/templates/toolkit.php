117a4171e62912df31f7d12f21f10afd4af4b312
Arthur Schmitt <tart2000design@gmail.com>
2018-04-25 10:08:09 +0200

    added on category
    + better toolkit page

--- site/templates/toolkit.php
+++ site/templates/toolkit.php
@@ -1,31 +1,30 @@
 <?php snippet('header') ?>
 
-<div class="row">
+<div class="row mt">
 	<?= $page->description()->kirbytext() ?>
 </div>
 
-<div class="row mt toolkit">
-	<div class="col-md-3">
-		<div class="list-group list-group-flush">
-			<?php foreach ($page->children() as $cat) : ?>
-				<a href="<?= $cat->url() ?>" class="list-group-item list-group-item-action">
-					<?= $cat->title() ?>
-				</a>
-			<?php endforeach ?>
-		</div>
-	</div>
-
-	<!-- Bouton pour suggérer un outil -->
-
-	<?php $tools = $page->children()->children()->limit(10) ?>
-
-	<div class="col-md-9">
-		<ul class="list-unstyled">
-			<?php foreach ($tools as $tool) : ?>
-				<?php snippet('media-item', array('tool' => $tool)) ?>
-			<?php endforeach ?>
-		</ul>
-	</div>
+<div clas="bmt">
+	<div class="card-columns">
+		<?php foreach ($page->children() as $cat) : ?>
+			<div class="card mb-4">
+	  			<div class="card-header text-center">
+	  				<a href="<?= $cat->url() ?>">
+				    	<h5 class="mb-0"><?= $cat->title() ?> (<?= $cat->children()->count() ?>)</h5> 
+				    </a>
+				</div>
+				<div class="card-body pb-3">
+				  	<?php foreach ($cat->children() as $tool ) : ?>
+				  		<?php if ($tool->hasImages()) : ?>
+				  			<a href="<?= $tool->theurl() ?>" target="_blank">
+							<img class="rounded-thumb p-2 lines" src="<?= $tool->images()->first()->url() ?>" alt="Generic placeholder image">
+							</a>
+						<?php endif ?>
+				    <?php endforeach ?>
+				</div>
+			</div> <!-- end card -->
+  		<?php endforeach ?>
+  	</div>
 </div>
 
 <?php snippet('footer') ?>
